2135ff20e5a7bd058a5361de0067c1ec0cb10ad7
Sharp0802 <nrs070802@gmail.com>
2021-03-16 12:49:12 +0900

    Change IProvider spec

--- QuietOffliner.Core/Controller/IProvider.cs
+++ QuietOffliner.Core/Controller/IProvider.cs
@@ -12,10 +12,11 @@ namespace QuietOffliner.Core.Controller
         public string HomePageUri { get; }
 
         public Task<Request<Episode?>> LoadEpisode(string id);
-        public Task<Request<ImmutableArray<EpisodeInfo>>> LoadEpisodeInfos(string query);
-        public Task<Request<ImmutableArray<EpisodeInfo>>> LoadRecentEpisodeInfos();
+        public Task<Request<ImmutableArray<EpisodeInfo>>> LoadEpisodeInfos(string query, int page = 0);
+        public Task<Request<ImmutableArray<EpisodeInfo>>> LoadRecentEpisodeInfos(int page = 0);
         
-        public Task<Request<ImmutableArray<SeriesInfo>>> LoadRecentSeriesInfos();
-        public Task<Request<ImmutableArray<SeriesInfo>>> LoadAllSeriesInfos(SearchRequest request);
+        public Task<Request<SeriesInfo?>> LoadSeriesInfo(string query, int page = 0);
+        public Task<Request<ImmutableArray<SeriesInfo>>> LoadRecentSeriesInfos(int page = 0);
+        public Task<Request<ImmutableArray<SeriesInfo>>> LoadAllSeriesInfos(SearchRequest request, int page = 0);
     }
 }
