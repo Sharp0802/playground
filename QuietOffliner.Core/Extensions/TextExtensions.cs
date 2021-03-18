f614f1f949c385db2a11bf78a0a1b27ec5a4428f
Sharp0802 <nrs070802@gmail.com>
2021-03-18 21:15:42 +0900

    Add Extensions

---
+++ QuietOffliner.Core/Extensions/TextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace QuietOffliner.Core.Extensions
+{
+	public static class TextExtensions
+	{
+		public static string ToString(this byte[] bytes, Encoding? encoding)
+		{
+			encoding ??= Encoding.UTF8;
+
+			return encoding.GetString(bytes);
+		}
+
+		public static Encoding GetEncoding(this string encoding)
+		{
+			try
+			{
+				return Encoding.GetEncoding(encoding);
+			}
+			catch
+			{
+				return Encoding.UTF8;
+			}
+		}
+	}
+}
