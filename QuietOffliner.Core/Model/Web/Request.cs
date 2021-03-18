eb17826dea3c08795a0af127e4ad46ac884aff61
Sharp0802 <nrs070802@gmail.com>
2021-03-18 21:19:21 +0900

    Add IsSucceed Property

--- QuietOffliner.Core/Model/Web/Request.cs
+++ QuietOffliner.Core/Model/Web/Request.cs
@@ -4,18 +4,20 @@ namespace QuietOffliner.Core.Model.Web
 {
     public readonly struct Request<TRequest>
     {
-        private Request(TRequest? value, HttpStatusCode code)
+        private Request(TRequest? value, HttpStatusCode code, bool isSucceed)
         {
             Value = value;
             ResponseCode = code;
+            IsSucceed = isSucceed;
         }
         
         public static Request<TRequest> Failed(HttpStatusCode code)
-            => new(default, code);
+            => new(default, code, false);
         public static Request<TRequest> Successful(TRequest result, HttpStatusCode code)
-            => new(result, code);
+            => new(result, code, true);
         
         public TRequest?        Value { get; }
         public HttpStatusCode   ResponseCode { get; }
+        public bool             IsSucceed { get; }
     }
 }
