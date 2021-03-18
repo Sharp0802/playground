0627a76bb1b319d5ee749b23c4d34a0960f24795
Sharp0802 <nrs070802@gmail.com>
2021-03-18 17:54:49 +0900

    Add ProviderSupportList

---
+++ QuietOffliner.Core/Controller/ProviderSupportList.cs
@@ -0,0 +1,32 @@
+﻿namespace QuietOffliner.Core.Controller
+{
+	public struct ProviderSupportList
+	{
+		public bool CanLogin;
+		
+		public bool EpisodeInfosPageIndexing;
+		public bool RecentEpisodeInfosPageIndexing;
+		
+		public bool CanLoadSeriesInfo;
+		public bool SeriesInfosPageIndexing;
+		public bool RecentSeriesInfosPageIndexing;
+
+		public ProviderSupportList(
+			bool canLogin,
+			
+			bool episodeInfosPageIndexing,
+			bool recentEpisodeInfosPageIndexing,
+			
+			bool canLoadSeriesInfo,
+			bool seriesInfosPageIndexing,
+			bool recentSeriesInfosPageIndexing)
+		{
+			CanLogin = canLogin;
+			EpisodeInfosPageIndexing = episodeInfosPageIndexing;
+			RecentEpisodeInfosPageIndexing = recentEpisodeInfosPageIndexing;
+			CanLoadSeriesInfo = canLoadSeriesInfo;
+			SeriesInfosPageIndexing = seriesInfosPageIndexing;
+			RecentSeriesInfosPageIndexing = recentSeriesInfosPageIndexing;
+		}
+	}
+}
