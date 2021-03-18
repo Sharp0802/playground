691ba0cb7c42e96c58ed6a7a4118ef07986a2fe2
Sharp0802 <nrs070802@gmail.com>
2021-03-18 19:09:25 +0900

    Add abstract class Provider instead of IProvider

--- QuietOffliner.Core/Controller/IProvider.cs
+++ QuietOffliner.Core/Controller/IProvider.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Immutable;
+using System.IO;
 using System.Threading.Tasks;
+using QuietOffliner.Core.Controller.Web;
 using QuietOffliner.Core.Model;
 using QuietOffliner.Core.Model.SearchInfos;
 using QuietOffliner.Core.Model.Web;
@@ -19,5 +21,33 @@ namespace QuietOffliner.Core.Controller
         public Task<Request<SeriesInfo?>> LoadSeriesInfo(string query);
         public Task<Request<ImmutableArray<SeriesInfo>>> LoadRecentSeriesInfos(int page = 0);
         public Task<Request<ImmutableArray<SeriesInfo>>> LoadAllSeriesInfos(SearchRequest request, int page = 0);
+    }
+    
+    public abstract class Provider : IProvider
+    {
+        protected Provider(
+            string name,
+            string homePage,
+            ProviderSupportList supportList,
+            TextWriter? logStream = null)
+        {
+            Name = name;
+            HomePageUri = homePage;
+            SupportList = supportList;
+            
+            Client = new WebRequestClient(logStream);
+        }
+
+        public string Name { get; }
+        public string HomePageUri { get; }
+        public ProviderSupportList SupportList { get; }
+        protected WebRequestClient Client { get; }
+
+        public abstract Task<Request<Episode?>> LoadEpisode(string id);
+        public abstract Task<Request<ImmutableArray<EpisodeInfo>>> LoadEpisodeInfos(string query, int page = 0);
+        public abstract Task<Request<ImmutableArray<EpisodeInfo>>> LoadRecentEpisodeInfos(int page = 0);
+        public abstract Task<Request<SeriesInfo?>> LoadSeriesInfo(string query);
+        public abstract Task<Request<ImmutableArray<SeriesInfo>>> LoadRecentSeriesInfos(int page = 0);
+        public abstract Task<Request<ImmutableArray<SeriesInfo>>> LoadAllSeriesInfos(SearchRequest request, int page = 0);
     }
 }
