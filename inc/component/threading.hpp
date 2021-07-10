3bb2e8b624150ca9f8c9c40f34a4d565b7f0d3c3
Sharp0802 <nrs070802@gmail.com>
2021-07-10 13:54:34 +0900

    Add delay func

--- inc/component/threading.hpp
+++ inc/component/threading.hpp
@@ -1,12 +1,14 @@
 #ifndef THREADING_HPP
 #define THREADING_HPP
 
+#include "internal.hpp"
 
 namespace Probo::Threading
 {
 	class Thread
 	{
-
+	public:
+		static void Sleep(unsigned int ms);
 	};
 }
 
