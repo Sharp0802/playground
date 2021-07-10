32f02f0290b3d767ec1dc8a52604e1d2007d4edc
Sharp0802 <nrs070802@gmail.com>
2021-07-10 14:13:11 +0900

    Add error handler/noticer

--- src/component/lifetime.cpp
+++ src/component/lifetime.cpp
@@ -1,17 +1,32 @@
 
+#include "component/threading.hpp"
 #include "component/lifetime.hpp"
-#include "component/internal.hpp"
+#include "component/audio.hpp"
 
+using namespace Probo::Audio;
 using namespace Probo::Lifetime;
+using namespace Probo::Threading;
 
 int MainInitializer::Activate()
 {
-	Init();
-	start();
-	Main();
-	end();
-	Dispose();
-	return 0;
+	try
+	{
+		Init();
+		start();
+		Main();
+		end();
+		Dispose();
+		return 0;
+	}
+	catch (...)
+	{
+		for (unsigned char i = 0; i < 3; ++i)
+		{
+			Buzzer::Play(WaveType::Do, 4, 100);
+			Thread::Sleep(100)
+		}
+		return -1;
+	}
 }
 
 MainInitializer::MainInitializer(void (*init)(), void (*main)(), void (*dispose)())
