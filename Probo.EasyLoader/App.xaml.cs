dab62c824d1b2cfccaad380e45a127807b6c2dec
Sharp0802 <sharp0802.business@gmail.com>
2021-12-18 16:12:50 +0900

    Add project files.

---
+++ Probo.EasyLoader/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Probo.EasyLoader
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            new MainWindow().Activate();
+        }
+    }
+}
