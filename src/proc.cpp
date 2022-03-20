cfdb2ae4e17ea851c73a2a88948921f6301e4db4
Sharp0802 <sharp0802.business@gmail.com>
2022-03-20 20:06:44 +0900

    Impl shell-command executor

---
+++ src/proc.cpp
@@ -0,0 +1,18 @@
+#include "proc.hpp"
+
+#include <array>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+
+std::string proc::exec(const std::string& cmd)
+{
+	std::array<char, 256> buf;
+	std::stringstream stream;
+	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+	if (!pipe)
+		throw std::runtime_error("failed to run popen()");
+	while (fgets(buf.data(), buf.size(), pipe.get()) != nullptr)
+		stream << buf.data();
+	return stream.str():
+}
