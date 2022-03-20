29665c99a512ba07a7b7a59c163ae5d5f7d0f22d
Sharp0802 <sharp0802.business@gmail.com>
2022-03-20 20:11:01 +0900

    Make shell execution test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 #include "com.hpp"
+#include "proc.hpp"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+	std::cout << proc::exec("echo 'Hello World!'");
     return 0;
 }
