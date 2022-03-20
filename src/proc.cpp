3ec30fb247f3e3be0dcad61f7029157615eb9ede
Sharp0802 <sharp0802.business@gmail.com>
2022-03-20 20:09:39 +0900

    Fix compile error

--- src/proc.cpp
+++ src/proc.cpp
@@ -7,12 +7,12 @@
 
 std::string proc::exec(const std::string& cmd)
 {
-	std::array<char, 256> buf;
+	std::array<char, 256> buf{};
 	std::stringstream stream;
-	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
 	if (!pipe)
 		throw std::runtime_error("failed to run popen()");
 	while (fgets(buf.data(), buf.size(), pipe.get()) != nullptr)
 		stream << buf.data();
-	return stream.str():
+	return stream.str();
 }
